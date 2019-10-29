788d780c4fc2af9a17386d3ccbe43ef8d7120436
sonota88 <yosiot8753@gmail.com>
2019-10-30 07:14:38 +0900

    TableTools.from_jatable

--- lib/table_tools.rb
+++ lib/table_tools.rb
@@ -2,11 +2,17 @@ require "table_tools/version"
 require 'table_tools/data_frame'
 
 autoload :Mrtable, 'mrtable'
+autoload :JsonArrayTable, 'json_array_table'
 
 module TableTools
 
   def self.from_mrtable(src)
     colnames, rows = Mrtable.parse(src)
+    DataFrame.new(colnames, rows)
+  end
+
+  def self.from_jatable(src)
+    colnames, rows = JsonArrayTable.parse(src)
     DataFrame.new(colnames, rows)
   end
 
--- test/table_tools_test.rb
+++ test/table_tools_test.rb
@@ -20,4 +20,20 @@ class TableToolsTest < Minitest::Test
     assert_equal(nil, df.rows[0][1])
   end
 
+  def test_from_jatable
+    jatable_src = <<-EOB
+[ "c1", "c2"  ]
+[ "1", null ]
+    EOB
+
+    df = TableTools.from_jatable(jatable_src)
+    # pp df
+
+    assert_equal("c1", df.colnames[0])
+    assert_equal("c2", df.colnames[1])
+
+    assert_equal("1", df.rows[0][0])
+    assert_equal(nil, df.rows[0][1])
+  end
+
 end
