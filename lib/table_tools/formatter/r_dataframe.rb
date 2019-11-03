8357844d78228e96bfc5bfe73c970bda89effd3e
sonota88 <yosiot8753@gmail.com>
2019-11-03 19:38:03 +0900

    Add TableTools::Formatter::RDataframe

---
+++ lib/table_tools/formatter/r_dataframe.rb
@@ -0,0 +1,24 @@
+module TableTools
+  module Formatter
+    class RDataframe
+      def self.format(colnames, rows)
+        s = "data.frame("
+
+        colnames.each_with_index{|cn, i|
+          head = (i == 0) ? "  " : "  ,"
+
+          values = rows.map{|cols|
+            cols[i]
+          }.map{|col|
+            col.nil? ? "NA" : '"' + col + '"'
+          }.join(", ")
+
+          s += "\n#{head}#{cn} = as.numeric(c(#{values}))"
+        }
+
+        s += "\n)"
+        s + "\n"
+      end
+    end
+  end
+end
