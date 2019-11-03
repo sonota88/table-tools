8e74f54c15dfa34ea107284ca0427fd7dda06a74
sonota88 <yosiot8753@gmail.com>
2019-11-03 19:40:15 +0900

    Add TableTools::Parser::PostgresParser

---
+++ lib/table_tools/parser/postgres_parser.rb
@@ -0,0 +1,32 @@
+module TableTools
+  module Parser
+    module PostgresParser
+      def self.parse(src)
+        lines = src.split("\n")
+        rows =
+          lines
+            .select{|line| /^ / =~ line }
+            .map{|line|
+              cols = line.split(" | ")
+              cols
+                .map{|col| col.strip }
+                .map{|col| col == "NULL" ? nil : col }
+            }
+        rows
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  require "pp"
+
+  src = <<-EOB
+ id | name
+----+-------
+ 1  | foo
+ 2  | NULL
+  EOB
+  rows = TableTool::Parser::PostgresParser.parse(src)
+  pp rows
+end
