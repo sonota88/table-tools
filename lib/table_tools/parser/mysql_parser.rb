3711496e3cd74f846a666e9d56ac7bdcfb902f79
sonota88 <yosiot8753@gmail.com>
2019-11-03 19:39:29 +0900

    Add TableTools::Parser::MysqlParser

---
+++ lib/table_tools/parser/mysql_parser.rb
@@ -0,0 +1,37 @@
+module TableTools
+  module Parser
+    module MysqlParser
+      def self.parse(src)
+        # $stderr.puts [123456, src]
+        lines = src.split("\n")
+        rows =
+          lines
+            .select{|line| /^\+\-/ !~ line }
+            .map{|line|
+              cols = " #{line} ".split(" | ")
+              cols
+                .map{|col| col.strip }[1..-1]
+                .map{|col| col == "NULL" ? nil : col }
+            }
+        head = rows[0]
+        [head] + rows[1..-1]
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  require "pp"
+
+  src = <<-EOB
++-------+--------+
+| col1  | col2   |
++-------+--------+
+| 1     | 22     |
+| 12.34 | NULL   |
+|       | null   |
++-------+--------+
+  EOB
+  rows = TableTool::Parser::MysqlParser.parse(src)
+  pp rows
+end
