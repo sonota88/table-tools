e87cf14f6fb0c77ad217c6a8d37e09730880037e
sonota88 <yosiot8753@gmail.com>
2019-11-03 19:57:58 +0900

    git add bin/jatable-formatter.rb

---
+++ bin/jatable-formatter.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(
+  File.join(__dir__, "../lib")
+)
+
+require 'table_tools'
+
+src = $stdin.read
+
+begin
+  df = TableTools.from_jatable(src, complement: nil)
+  print df.to_jatable()
+rescue => e
+  puts src
+  puts "----"
+  puts e.message, e.class, e.backtrace
+  puts "----"
+end
